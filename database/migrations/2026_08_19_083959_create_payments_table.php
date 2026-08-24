1710a7abdb9efcd1124ce63022f98decc26f8425
yosra200 <yosrakhattab2020@gmail.com>
2026-08-24 11:23:50 +0300

    update user table

--- database/migrations/2026_08_19_083959_create_payments_table.php
+++ database/migrations/2026_08_19_083959_create_payments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('payments', function (Blueprint $table) {
+        Schema::create('paymentss', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('order_id')
