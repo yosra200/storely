af852f0ffe7403cfdcff07fe539a59ebbd318798
yosra200 <yosrakhattab2020@gmail.com>
2026-09-09 11:42:30 +0300

    update

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,21 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'role' => $this->role,
+            'image' => $this->image,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'image' => $this->image,
+            'is_active' => $this->is_active,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
